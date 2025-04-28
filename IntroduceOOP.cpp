07e69cdc4a5c8ec329174f1babfe0911d9eec642
maulinakhamidah <maulinakhamidah115@gmail.com>
2025-04-28 18:24:22 +0700

    membuat prosedurOOP

---
+++ IntroduceOOP.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+class panet{
+   private:
+       float gravitasi;
+   public:
+       string nama; 
+       float diameter;
+
+       void displaydata(){
+        cout << "halo aku planet " << nama << ", ukuranku "<< diameter <<", gravitasiku "<< gravitasi << endl;
+    
+       }
+       void setdata(float g){
+        
+       }
+
+};
+int main()
+{
+
+}
